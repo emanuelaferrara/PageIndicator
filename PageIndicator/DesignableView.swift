1d901f3c95c0983696362f07751a341ff752d83a
Emanuela Ferrara <emanuelaferrara@hotmail.com>
2019-01-31 18:39:37 +0100

    new gradient view

---
+++ PageIndicator/DesignableView.swift
@@ -0,0 +1,50 @@
+//
+//  DesignableView.swift
+//  PageIndicator
+//
+//  Created by Emanuela Ferrara on 31/01/2019.
+//  Copyright © 2019 Emanuela. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableView: UIView
+{
+    @IBInspectable var gradientColor1: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientColor2: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientStartPoint: CGPoint = .zero {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientEndPoint: CGPoint = CGPoint(x: 0, y: 1) {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    private func setGradient()
+    {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+        gradientLayer.startPoint = self.gradientStartPoint
+        gradientLayer.endPoint = self.gradientEndPoint
+        gradientLayer.frame = self.bounds
+        if let topLayer = self.layer.sublayers?.first, topLayer is CAGradientLayer
+        {
+            topLayer.removeFromSuperlayer()
+        }
+        self.layer.addSublayer(gradientLayer)
+    }
+}
